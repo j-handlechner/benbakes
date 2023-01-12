44f56221bdfa778f89a7afababb86cbf7041c561
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-01-12 17:00:59 +0100

    added basic swipe functionality - no animation

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,7 +1,6 @@
 const btn = document.getElementById("hamburger")
 
 btn.addEventListener("click", () => {
-    console.log(btn.ariaLabel)
     if(btn.classList == "closed") {
         btn.classList.remove("closed");
         btn.classList.add("opened");
@@ -22,3 +21,39 @@ for(let el of navElements) {
 }
 
 document.querySelector("nav > ul").classList.add("transition")
+
+const slider_left = document.getElementById("left_slider")
+const slider_right = document.getElementById("right_slider")
+const container = document.getElementById("slidable-container")
+console.log(container)
+console.log(container.classList)
+let counter = 0
+
+slider_right.addEventListener("click", () => {
+    console.log(container.classList)
+    
+    switch(counter%3){
+        case 0:
+            console.log("case0 is activated")
+            console.log(counter)
+            if(container.classList.contains("state2")){container.classList.remove("state2")}
+            container.classList.add("state0")
+            counter++
+            break
+        case 1: 
+            console.log("case1 is activated")
+            console.log(counter)
+            container.classList.remove("state0")
+            container.classList.add("state1")
+            counter++
+            break
+        case 2: 
+            console.log("case2 is activated")
+            console.log(counter)
+            container.classList.remove("state1")
+            container.classList.add("state2")
+            counter++
+            break
+    }
+    
+})
