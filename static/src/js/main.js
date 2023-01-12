f069665c54d4a1465abaf15957f371a003b5750e
Denis Stjepanovic <stjepanovic.dns@gmail.com>
2023-01-12 17:49:14 +0100

    slider done

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -25,35 +25,43 @@ document.querySelector("nav > ul").classList.add("transition")
 const slider_left = document.getElementById("left_slider")
 const slider_right = document.getElementById("right_slider")
 const container = document.getElementById("slidable-container")
-console.log(container)
-console.log(container.classList)
+
 let counter = 0
 
 slider_right.addEventListener("click", () => {
-    console.log(container.classList)
-    
     switch(counter%3){
         case 0:
-            console.log("case0 is activated")
-            console.log(counter)
+            slider_left.classList.remove("left-disappear")
+            slider_left.classList.add("left-appear")
             if(container.classList.contains("state2")){container.classList.remove("state2")}
             container.classList.add("state0")
             counter++
             break
         case 1: 
-            console.log("case1 is activated")
-            console.log(counter)
+            slider_right.classList.remove("right-appear")
+            slider_right.classList.add("right-disappear")
             container.classList.remove("state0")
             container.classList.add("state1")
             counter++
             break
-        case 2: 
-            console.log("case2 is activated")
-            console.log(counter)
-            container.classList.remove("state1")
+    }
+})
+
+slider_left.addEventListener("click", () => {
+    switch(counter%3){
+        case 1: 
+            slider_left.classList.remove("left-appear")
+            slider_left.classList.add("left-disappear")
+            container.classList.remove("state0")
             container.classList.add("state2")
-            counter++
+            counter--
+            break
+        case 2:
+            slider_right.classList.remove("right_disappear")
+            slider_right.classList.add("right-appear")
+            container.classList.remove("state1")
+            container.classList.add("state0")
+            counter--
             break
     }
-    
 })
