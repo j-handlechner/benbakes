50c08efc60d3006f52bfd6a4722e39241a6ca924
Johannes Handlechner <jhandlechner.lba@fh-salzburg.ac.at>
2023-01-23 16:28:40 +0100

    clean up functions.php

--- wordpress/wp-content/themes/benbakes/functions.php
+++ wordpress/wp-content/themes/benbakes/functions.php
@@ -2,9 +2,7 @@
 
 function add_theme_scripts_and_styles() {
 	wp_enqueue_style("style", get_stylesheet_uri());
-	// wp_enqueue_script( 'main', get_template_directory_uri());
 	wp_enqueue_script( 'main', get_template_directory_uri() . '/main.js', array(), 1, true);
-	// wp_enqueue_script( 'main-nav', get_template_directory_uri() . '/js/main-nav.js');
 }
 
 add_action('wp_enqueue_scripts', 'add_theme_scripts_and_styles');
